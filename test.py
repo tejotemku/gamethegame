cd8718c83c99f49268facae266c27b6773c7f839
Filip Mirosław Drygaś <f.drygas@stud.elka.pw.edu.pl>
2020-01-21 19:24:47 +0100

    testy

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 from game import Game
+from location import Location
 
 
 def test_loading():
     game = Game()
     game.load_map("game save")
     assert game.map
+
+
+def test_location():
+    dict_location = {
+        "id": 33,
+        "name": "Town",
+        "type": "vanilla",
+        "description": "test string",
+        "hidden items": [],
+        "locations": [],
+        "key": None
+    }
+    location = Location(dict_location)
+    assert location.id == 33
+    assert location.name == "Town"
